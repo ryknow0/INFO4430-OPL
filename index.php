72fa0b0e995e5debfb6cb2e2f26fe74df8bbccd5
Ryan <ryknow0@gmail.com>
2019-05-22 18:38:30 -0600

    Login page html added

--- index.php
+++ index.php
@@ -2,11 +2,80 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Orem Library Event Tracker</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta content="Webflow" name="generator">
+    <link href="/images/opl_logo.jpg" rel="stylesheet" type="text/css">
+    <script type="text/javascript">
+    !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
+    </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/images/site.webmanifest">
+    <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 </head>
+
 <body>
-<h1> Git Commit Test</h1>    
+    <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+        <div class="container-2 w-container"><a href="/" class="w-nav-brand w--current">
+        <img src="https://assets.website-files.com/5ce2009f7c5e2f0115026bff/5ce216030b5f0bfe3825a371_orem%20library.jpg" alt="" class="navbar-image"></a>
+        <div class="w-nav-button">
+            <div class="w-icon-nav-menu">
+
+            </div>
+        </div>
+    </div>
+    <div class="w-nav-overlay" data-wf-ignore="">
+
+    </div>
+</div>
+<div class="page-wrapper">
+    <div class="container w-container">
+        <div class="div-block">
+            <img src="https://assets.website-files.com/5ce2009f7c5e2f0115026bff/5ce216a30b5f0b239325a400_EventTracker_Logo%20(1).png" alt="" class="logo-image">
+            <h1 class="heading">Event<br>Tracker</h1>
+        </div>
+        <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form" class="form">
+                <div class="field-wrapper">
+                    <label for="name" class="field-label">Username: </label>
+                    <input type="text" class="form-field w-input" maxlength="256" name="name" data-name="Name" id="name">
+                </div>
+                <div class="field-wrapper">
+                    <label for="name-2" class="field-label">Password:</label>
+                    <input type="text" class="form-field w-input" maxlength="256" name="name-2" data-name="Name 2" id="name-2">
+                </div>
+                <div class="w-clearfix">
+                    <a href="#" class="forgot-password-link">forgot password?</a>
+                </div>
+                <div class="form-button-wrapper">
+                    <input type="submit" value="Login" data-wait="Please wait..." class="button login w-button">
+                    <input type="submit" value="Sign Up" data-wait="Please wait..." class="button w-button">
+                </div>
+            </form>
+            <div class="w-form-done">
+                <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+                <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+    <a href="/create-account" class="link">Create Account</a>
+    <a href="/create-event" class="link">Create Event</a>
+    <a href="/add-user" class="link">Add User</a>
+    <a href="/generate-report" class="link">Generate Report</a>
+    <a href="/admin-page" class="link">Admin Page</a>
+    <a href="/event-tracker" class="link">Event Tracker</a>
+</div>
+<script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://assets.website-files.com/5ce2009f7c5e2f0115026bff/js/webflow.f0833e518.js" type="text/javascript"></script>
+<!--[if lte IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->    
 </body>
 </html>
