7025a0cf75c9f4aaf3f27cae08e00634d3c0bc0c
Ryan Conklin <ryknow0@gmail.com>
2019-06-11 20:04:36 -0600

    basic MVC file placeholders added

---
+++ model/database.php
@@ -0,0 +1,18 @@
+<?php
+   mb_internal_encoding('UTF-8');
+   mb_http_output('UTF-8');
+   mb_http_input('UTF-8');
+   mb_regex_encoding('UTF-8');
+   $dsn = 'mysql:host=localhost;dbname=event_tracker';
+   $username = 'remoteuser';
+   $password = 'remoteuser0!';
+  
+   try {
+       //db = global variable it is declared here and used other plces on the site
+       $db = new PDO($dsn, $username, $password);
+   } catch (PDOException $e) {
+       $error_message = $e->getMessage();
+       echo 'Error Message ' . $error_message;
+       exit();
+   }
+?>
