560dc0ddd1f3f31a9d37a05d0b4927b506e5125b
Ryan Conklin <ryknow0@gmail.com>
2019-06-12 00:22:22 -0600

    index.html fix

---
+++ view/footer.php
@@ -0,0 +1,13 @@
+<<!-- <footer>
+    <p class="copyright">
+        &copy; <?php //echo date("Y"); ?> My Guitar Shop, Inc.
+    </p>
+</footer>
+</body>
+</html> -->
+<footer>
+  <a href="http://localhost/INFO3426/Assignment_6/index.php">Home</a><p class="copyright">&copy; <?php echo date("Y"); ?> The Book Loan System</p>
+</footer>
+<a href="http://localhost/INFO3426/Assignment_6/logout.php">Logout</a>
+</body>
+</html>
--- view/header.php
+++ view/header.php
@@ -1,3 +1,31 @@
-<link rel="stylesheet" href="styles.css">
-<img src="mandala.jpg" height="50" width="50">
-<h1 id="title">RDC Memory Madness with Memero</h1>
+<!-- <!DOCTYPE html>
+<html> -->
+<!-- the head section -->
+<!-- <head>
+    <title>RDC My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/INFO3426/assign4/main.css">
+</head> -->
+
+<!-- the body section -->
+<!-- <body>
+<header><h1>My Guitar Shop</h1></header> -->
+
+<?php
+   if ( !isset($_SESSION['Email']) ) {
+       header("location:loginform.php");
+   }
+   $first_name = $_SESSION['Firstname'];
+   $last_name = $_SESSION['Lastname'];
+?>
+<!DOCTYPE html>
+<html>
+<!-- the head section /INFO3426/Assignment_6-->
+<head>
+  <title>OPL - Event Tracker</title>
+  <link rel="stylesheet" type="text/css" href="http://localhost/INFO4430/styles.css">
+</head>
+
+<!-- the body section -->
+<body>
+Welcome <?=$first_name?> <?=$last_name?>
+<header><h1>The Book Loan System</h1> </header>
