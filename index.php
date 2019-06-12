1ba872c284954a5de0a53140e5bcbd632d4b8866
Ryan Conklin <ryknow0@gmail.com>
2019-06-12 01:00:29 -0600

    index.php update webflow css filepath

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'view/header.php';
 ?>
 <body>
   <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
-    <div class="container-2 w-container"><a href="index.html" class="w-nav-brand w--current"><img src="images/orem-library.jpg" alt="" class="navbar-image"></a>
+    <div class="container-2 w-container"><a href="index.html" class="w-nav-brand"><img src="images/orem-library.jpg" alt="" class="navbar-image"></a>
       <div class="w-nav-button">
         <div class="w-icon-nav-menu"></div>
       </div>
@@ -12,24 +12,35 @@ include 'view/header.php';
   </div>
   <div class="page-wrapper">
     <div class="container w-container">
-      <div class="div-block"><img src="images/EventTracker_Logo-1.png" alt="" class="logo-image">
-        <h1 class="heading">Event<br>Tracker</h1>
-      </div>
-      <div class="w-form">
-        <form id="email-form" name="email-form" data-name="Email Form" class="form">
-          <div class="field-wrapper"><label for="name" class="field-label">Username: </label><input type="text" class="form-field w-input" maxlength="256" name="name" data-name="Name" id="name"></div>
-          <div class="field-wrapper"><label for="name-2" class="field-label">Password:</label><input type="text" class="form-field w-input" maxlength="256" name="name-2" data-name="Name 2" id="name-2"></div>
-          <div class="w-clearfix"><a href="#" class="forgot-password-link">forgot password?</a></div>
-          <div class="form-button-wrapper"><input type="submit" value="Login" data-wait="Please wait..." class="button login w-button"><input type="submit" value="Sign Up" data-wait="Please wait..." class="button w-button"></div>
-        </form>
-        <div class="w-form-done">
-          <div>Thank you! Your submission has been received!</div>
+      <h1 class="centered-h1">Welcome, Jono</h1>
+      <div class="columns w-row">
+        <div class="w-col w-col-4 w-col-small-small-stack">
+          <h3 class="h3">Top Events</h3>
         </div>
-        <div class="w-form-fail">
-          <div>Oops! Something went wrong while submitting the form.</div>
+        <div class="w-col w-col-4 w-col-small-small-stack">
+          <h3 class="h3">YTD Attendance</h3>
+          <h1 class="h1-huge">1895</h1>
+        </div>
+        <div class="w-col w-col-4 w-col-small-small-stack">
+          <h3 class="h3">YTD Events</h3>
+          <h1 class="h1-huge">12</h1>
+        </div>
+      </div>
+      <div class="columns w-row">
+        <div class="w-col w-col-7">
+          <div class="bold-text">Upcoming Events</div>
+        </div>
+        <div class="w-col w-col-5">
+          <div class="centered-contents-div"><a href="#" class="button fixed-width-button w-button">Generate Report</a></div>
+          <div class="centered-contents-div"><a href="#" class="button fixed-width-button w-button">Create Event</a></div>
+          <div class="centered-contents-div"><a href="#" class="button fixed-width-button w-button">Add User</a></div>
         </div>
       </div>
     </div>
   </div>
-  <div><a href="create-account.html" class="link">Create Account</a><a href="create-event.html" class="link">Create Event</a><a href="add-user.html" class="link">Add User</a><a href="generate-report.html" class="link">Generate Report</a><a href="admin-page.html" class="link">Admin Page</a><a href="event-tracker.html" class="link">Event Tracker</a></div>
+  <div><a href="create-account.html" class="link">Create Account</a><a href="create-event.html" class="link">Create Event</a><a href="add-user.html" class="link">Add User</a><a href="generate-report.html" class="link">Generate Report</a><a href="admin-page.html" class="link w--current">Admin Page</a><a href="event-tracker.html" class="link">Event Tracker</a></div>
+  <script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
 <?php include 'view/footer.php'; ?>
--- loginform.php
+++ loginform.php
@@ -1,28 +1,62 @@
 <?php
    session_start();
    if ( isset($_SESSION['username']) ) {
-       header('Location: http://localhost/INFO3426/Assignment_6/index.php');
+       header('Location: http://localhost/index.php');
    }
    $errors='';
 ?>
 <!DOCTYPE html>
 <html>
 <!-- the head section -->
+<<html data-wf-page="5ce2009f7c5e2f9884026c00" data-wf-site="5ce2009f7c5e2f0115026bff">
 <head>
-  <title>The Book Loan System Login Page</title>
-  <link rel="stylesheet" type="text/css" href="http://localhost/INFO3426/Assignment_6/styles.css">
+  <meta charset="utf-8">
+  <title>Orem Library Event Tracker</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/orem-library-event-tracker.webflow.css" rel="stylesheet" type="text/css">
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="https://daks2k3a4ib2z.cloudfront.net/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="https://daks2k3a4ib2z.cloudfront.net/img/webclip.png" rel="apple-touch-icon">
 </head>
 
 <!-- the body section -->
 <body>
-<br/><br/>
-<form action="http://localhost/INFO3426/Assignment_6/user/index.php" method="post">
-   Email: <input type="text" name="email" placeholder="Email" size="10">
-   Password: <input type="password" name="password" placeholder="Password" size="10">
-   <!-- hidden makes the action send via post instead of get -->
-   <input type="hidden" name="action" value="login"/>
-   <input type="submit" value="Submit" />
-   <div class="errors"><?=$errors?></div>
-</form>
+  <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+    <div class="container-2 w-container"><a href="index.html" class="w-nav-brand w--current"><img src="images/orem-library.jpg" alt="" class="navbar-image"></a>
+      <div class="w-nav-button">
+        <div class="w-icon-nav-menu"></div>
+      </div>
+    </div>
+  </div>
+  <div class="page-wrapper">
+    <div class="container w-container">
+      <div class="div-block"><img src="images/EventTracker_Logo-1.png" alt="" class="logo-image">
+        <h1 class="heading">Event<br>Tracker</h1>
+      </div>
+      <div class="w-form">
+        <form id="email-form" name="email-form" data-name="Email Form" class="form">
+          <div class="field-wrapper"><label for="name" class="field-label">Username: </label><input type="text" class="form-field w-input" maxlength="256" name="name" data-name="Name" id="name"></div>
+          <div class="field-wrapper"><label for="name-2" class="field-label">Password:</label><input type="text" class="form-field w-input" maxlength="256" name="name-2" data-name="Name 2" id="name-2"></div>
+          <div class="w-clearfix"><a href="#" class="forgot-password-link">forgot password?</a></div>
+          <div class="form-button-wrapper"><input type="submit" value="Login" data-wait="Please wait..." class="button login w-button"><input type="submit" value="Sign Up" data-wait="Please wait..." class="button w-button"></div>
+        </form>
+        <div class="w-form-done">
+          <div>Thank you! Your submission has been received!</div>
+        </div>
+        <div class="w-form-fail">
+          <div>Oops! Something went wrong while submitting the form.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div><a href="create-account.html" class="link">Create Account</a><a href="create-event.html" class="link">Create Event</a><a href="add-user.html" class="link">Add User</a><a href="generate-report.html" class="link">Generate Report</a><a href="admin-page.html" class="link">Admin Page</a><a href="event-tracker.html" class="link">Event Tracker</a></div>
+  <script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
 
 <?php include 'view/footer.php'; ?>
