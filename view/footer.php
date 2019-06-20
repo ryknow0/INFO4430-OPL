fc56baf14bdffdaf4166917b9506530714660c8a
root <root@info4430oldev.ad.uvu.edu>
2019-06-20 00:39:51 +0000

    updating navigation

--- view/footer.php
+++ view/footer.php
@@ -1,10 +1,10 @@
 <<!-- <footer>
     <p class="copyright">
-        &copy; <?php //echo date("Y"); ?> My Guitar Shop, Inc.
+        &copy; <?php //echo date("Y"); ?> Event Tracker
     </p>
 </footer>
 </body>
-</html> -->
+</html>
 <!-- <footer>
   <a href="http://localhost/INFO3426/Assignment_6/index.php">Home</a><p class="copyright">&copy; <?php echo date("Y"); ?> The Book Loan System</p>
 </footer>
@@ -18,7 +18,10 @@
     <a href="user/index.php" class="link">Add User</a>
     <a href="report/generate-report.html" class="link">Generate Report</a>
     <a href="user/index.php" class="link">Admin Page</a>
-    <a href="event/index.php" class="link">Event Tracker</a></div>
+    <a href="event/index.php" class="link">Event Tracker</a>
+    <a href="index.php" class="link">Home</a>
+    <a href="logout.php" class="link">Logout</a>
+</div>
 </footer>
 <script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
