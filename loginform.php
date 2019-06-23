e649f9b07ecd89bd769d675bc90ef4d7e001f1c5
root <root@info4430oldev.ad.uvu.edu>
2019-06-23 09:20:28 +0000

    potentially broken changes

--- loginform.php
+++ loginform.php
@@ -48,7 +48,7 @@
         <form id="email-form" name="email-form" data-name="Email Form" class="form" action="user/index.php" method="post">
           <div class="field-wrapper"><label for="name" class="field-label">Username: </label><input type="text" class="form-field w-input" maxlength="256" name="username" data-name="username" id="name" placeholder="Username"></div>
           <div class="field-wrapper"><label for="name-2" class="field-label">Password:</label><input type="password" class="form-field w-input" maxlength="256" name="password" data-name="Name 2" id="name-2" placeholder="Password"></div>
-          <div class="w-clearfix"><a href="#" class="forgot-password-link">forgot password?</a></div>
+          <div class="w-clearfix"><a href="password/#" class="forgot-password-link">forgot password?</a></div>
           <div class="form-button-wrapper"><input type="submit" onclick="window.location.href = 'index.php';" value="Login" data-wait="Please wait..." class="button login w-button"><input type="submit" value="Sign Up" onclick="window.location.href = 'account/index.php' data-wait="Please wait..." class="button w-button"><input type="hidden" name="action" value="login"/></div>
         </form>
         <div class="w-form-done">
