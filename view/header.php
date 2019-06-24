ab2988738b628670a1fd47c6c985994de3f02dae
root <root@info4430oldev.ad.uvu.edu>
2019-06-24 04:52:39 +0000

    stuff

--- view/header.php
+++ view/header.php
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-=======
-<!-- <!DOCTYPE html>
-<html> -->
-<!-- the head section /INFO3426/Assignment_6-->
-<!-- <head>
-  <title>OPL - Event Tracker</title>
-  <link rel="stylesheet" type="text/css" href="http://localhost/INFO4430/styles.css">
-</head> -->
-<!--
->>>>>>> c93bdbf09252555af69e54f7f690903382bbcfab
+
+
 <?php
 //   if ( !isset($_SESSION['Email']) ) {
 //      header("location:index.php");
 //   }
 //   $first_name = $_SESSION['First_Name'];
 //   $last_name = $_SESSION['Last_Niame'];
-?>-->
+?>
 
 <!DOCTYPE html>
 <!--  This site was created in Webflow. http://www.webflow.com  -->
@@ -26,11 +17,11 @@
   <title>Orem Library Event Tracker</title>
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <meta content="Webflow" name="generator">
-  <link href="css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="css/orem-library-event-tracker.webflow.css" rel="stylesheet" type="text/css">
+  <link href="http://10.52.2.69/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="http://10.52.2.69/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="http://10.52.2.69/css/orem-library-event-tracker.webflow.css" rel="stylesheet" type="text/css">
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="images/webclip.png" rel="apple-touch-icon">
+  <link href="http://10.52.2.69/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="http://10.52.2.69/images/webclip.png" rel="apple-touch-icon">
 </head>
