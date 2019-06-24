266e8cd56509a25b3ad21d54d28dd618768a3c78
Ryan <ryknow0@gmail.com>
2019-06-23 23:41:23 -0600

    updated user/index.php

--- loginform.php
+++ loginform.php
@@ -60,18 +60,4 @@
     </div>
   </div>
   <div>
-
-  <!--
-    <a href="account/index.php" class="link">Create Account</a>
-    <a href="event/create-event.html" class="link">Create Event</a>
-    <a href="user/add-user.html" class="link">Add User</a>
-    <a href="report/generate-report.html" class="link">Generate Report</a>
-    <a href="admin-page.html" class="link">Admin Page</a>
-    <a href="event/event-tracker.html" class="link">Event Tracker</a></div>
-  <script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="js/webflow.js" type="text/javascript"></script>-->
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
-
-<!--  </body> -->
-
 <?php include 'view/footer.php'; ?>
--- user/index.php
+++ user/index.php
@@ -1,29 +1,39 @@
 <?php
 session_start();
 require('../model/user_db.php');
+
+//Conditions to magange the various actions related to users
+//take 'action' from post and set to local $action variable
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
+    //If $action is NULL check get for 'action'
    $action = filter_input(INPUT_GET, 'action');
    if ($action == NULL) {
-       $action = 'list_users';
+       $action = 'user_add';
    }
 }
-if ($action == 'list_users') {
+if ($action == 'user_add') {
    $users = get_users();
-   include('user_list.php');
+   include('user_add.php');
 }
-elseif($action == 'login'){  
+//processes the login information from the loginform.php
+elseif($action == 'login'){
+    //grabs value from Post "Email and Password" sets them to variables for use in the function
    $email = filter_input(INPUT_POST,'Email');
    $password = filter_input(INPUT_POST,'Password');
+   //email and password are passed as parameters to login() and set to $user
    $user = login($email, $password);
-   var_dump($user);
-   if($user == NULL){
+   //var_dump($user);
+   if($user == NULL){//checks to see if $user is NOT empty
+        //if $user is empty redirect user back to the loginform.php page
        header("Location: ../loginform.php?errors=Missing login credentials.");
    } else{
+       //If $user is not NULL then user information is set in the $_SESSION
        $_SESSION['Email'] = $user['Email'];
        $_SESSION['First_Name'] = $user['First_Name'];
        $_SESSION['Last_Name'] = $user['Last_Name'];
        $_SESSION['UserID'] = $user['UserID'];
+       //user is redirected to index.php
        header('Location: ../index.php');
    }
 }
