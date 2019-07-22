b9fe28a5af9a173e2eda2777c525b71969617ea1
Ryan <ryknow0@gmail.com>
2019-07-22 01:21:55 -0600

    update to account and Library wall

--- model/user_db.php
+++ model/user_db.php
@@ -17,7 +17,7 @@ function login($email, $password){
    $query = 'SELECT UserID, First_Name, Last_Name, Email FROM Users WHERE Email = :Email AND Password = :Password';
    //db query gets passed to the prepare function
    $statement = $db->prepare($query);
-  
+
    $statement->bindParam(':Email', $email);
    $statement->bindParam(':Password', $password);   
    //execute passes the statement (query) and sends it across the connection to the db sever       
@@ -29,29 +29,22 @@ function login($email, $password){
    //var_dump($user);
    return $user;   
 }
-
+var_dump($_SESSION);
 //Add User Function
-function add_user(){
+function add_user($first_name, $last_name, $email, $department, $permissions, $password){
+   //get account ID
+   $accountID = $_SESSION['AccountID'];
    global $db;
-   $query = 'INSERT  First_Name, Last_Name, Email FROM Users';
+   $query = 'INSERT INTO User(Email, First_Name, Last_Name, Department, Permissions, Password, AccountID)
+   VALUES (:email, :first_name, :last_name, :department, :permissions, :password, :accountID)';
    $statement = $db->prepare($query);
-   $statement->bindParam(':', $email);
-   $statement->bindParam(':', $)
+   $statement->bindParam(':email', $email);
+   $statement->bindParam(':first_name', $first_name);
+   $statement->bindParam(':last_name', $last_name);
+   $statement->bindParam('primary_contact', $primary_contact);
+   $statement->bindParam(':department', $department);
+   $statement->bindParam(':accountID', $accountID);
    $statement->execute();
-   $users = $statement->fetchAll();
-   $statement->closeCursor();
-
-   $user_query = 'INSERT INTO User(Email, First_Name, Last_Name, Department, Permissions, Password, AccountID)
-   VALUES (:email, :first_name, :last_name, :department, :permissions, :password, $last_id)';
-$statement = $db-prepare($user_query);
-$statement->bindParam(':email', $email);
-$statement->bindParam(':first_name', $first_name);
-$statement->bindParam(':last_name', $last_name);
-$statement->bindParam('primary_contact', $primary_contact);
-$statement->bindParam(':department', $department);
-$statement->bindParam(':account_id', $last_id);
-$statement->execute();
-
    return $user;
 }
 //Delete User Function
--- user/user_login.php
+++ user/user_login.php
@@ -7,28 +7,28 @@ require('../model/user_db.php');
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     //If $action is NULL check get for 'action'
-   $action = filter_input(INPUT_GET, 'action');
-   if ($action == NULL) {
-       $action = 'login';
-   }
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'login';
+    }
 }
 if ($action == 'login'){
     //grabs value from Post "Email and Password" sets them to variables for use in the function
-   $email = filter_input(INPUT_POST,'username');
-   $password = filter_input(INPUT_POST,'password');
+    $email = filter_input(INPUT_POST,'username');
+    $password = filter_input(INPUT_POST,'password');
    //email and password are passed as parameters to login() and set to $user
-   $user = login($email, $password);
+    $user = login($email, $password);
    //var_dump($user);
-   if($user == NULL){//checks to see if $user is NOT empty
+    if($user == NULL){//checks to see if $user is NOT empty
         //if $user is empty redirect user back to the loginform.php page
-       header("Location: ../loginform.php?errors=Missing login credentials.");
-   } else{
+        header("Location: ../loginform.php?errors=Missing login credentials.");
+    } else{
        //If $user is not NULL then user information is set in the $_SESSION
-       $_SESSION['Email'] = $user['Email'];
-       $_SESSION['First_Name'] = $user['First_Name'];
-       $_SESSION['Last_Name'] = $user['Last_Name'];
-       $_SESSION['UserID'] = $user['UserID'];
+        $_SESSION['Email'] = $user['Email'];
+        $_SESSION['First_Name'] = $user['First_Name'];
+        $_SESSION['Last_Name'] = $user['Last_Name'];
+        $_SESSION['UserID'] = $user['UserID'];
        //user is redirected to index.php
-       header('Location: ../index.php');
-   }
+        header('Location: ../index.php');
+    }
 }
