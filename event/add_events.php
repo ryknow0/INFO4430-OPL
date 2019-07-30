a7a21bae4c1993ca765c40d4b3f1961a03a05a89
litzy-carbajal <51427136+litzy-carbajal@users.noreply.github.com>
2019-07-30 13:43:50 -0600

    index event update

--- event/add_events.php
+++ event/add_events.php
@@ -11,13 +11,9 @@
         Event Location: <input type="text" name="event_location" > <br>
         Category: <input type="text" name="category"> <br>
         Target_Audience: <input type="text" name="target_audience"> <br>
-        <!-- ASK IF WE WANT TO CREATE THE EVENT TYPE AS LIST OR TEXT!--> Event Type: <select name="event_type">
-        <?php
-        foreach($event_types as $event_types) : ?>
-        <option value="<?=$event_type[0]?>"> <?= $event_type[0]?> </option>
-        <?php endforeach ?>
-        </select>
-        <br>
+        Event_Type: <input type="text" name="event_type"> <br>
+        <!-- ASK IF WE WANT TO CREATE THE EVENT TYPE AS LIST OR TEXT! Event Type: <select name="event_type"> -->
+        
         Topic: <input type="text" name="topic"> <br>
         <input type="submit" value="Submit">
         
--- event/index.php
+++ event/index.php
@@ -17,4 +17,31 @@ if ($action == 'get_all_events') {
   //  $events = 
 //}
 
+else if ($action == 'add_event_data'){
+    $event_name = filter_input(INPUT_POST, 'event_name');
+    $event_date = filer_input(INPUT_POST, 'event_date');
+    $start_time = filter_input(INPUT_POST, 'start_time');
+    $end_time = filter_input(INPUT_POST, 'end_time');
+    $event_location = filter_input(INPUT_POST, 'event_location');
+    $category = filter_input(INPUT_POST, 'category');
+    $target_audience = filter_input(INPUT POST, 'target_audience');
+    $event_type = filter_input(INPUT_POST, 'event_type');
+    $topic = filter_input(INPUT_POST, 'topic');   
+}
+
+$record_added = create_event($event_name, $event_date, $start_time, $end_time, 
+                            $event_location, $category, $target_audience, $event_type
+                            $topic);
+$message = '';
+if($record_added > 0) {
+    $message = 'Event Added Successfully';
+} else {
+    $message = 'Error Adding Event';
+}
+$event = read_events();
+include('event_tracker.php');
+
+
+
+
 ?>
