fabdd06901f0fb13416175978a924c3d4fdefb20
Ryan <ryknow0@gmail.com>
2019-08-05 01:46:15 -0600

    account dashboard update 19

--- index.php
+++ index.php
@@ -33,20 +33,11 @@ include 'view/account_dashboard_header.php';
           <!-- Google Charts API or manual Table to display upcoming events on an account-->
           <div id="table_div" class="gtable">
           </div>
-          <!-- <div class="bold-text">Upcoming Events</div>
-          <div class="w-col w-col-4">
-          <table border="1">
-            <tr><td>Date</td><td>Event</td><td>Event Type</td><td>Action</td></tr> -->
-            <?php ?>
-          <!--<?php //foreach ($event as $event) ?>
-            <tr>
-               <?php //echo '<td width=\'400\'>'. $event['Name'] . 
-               // '</td><td width='\'400\'>'. $event['Date'] .
-               // '</td><td width='\'400\'>' . $event['Type'] .
-               //'</td><td>Edit | Delete</td>'; ?>
-            </tr>
-             <?php //endforeach; ?>-->
-          </table>
+          <div class="w-col w-col-7">
+          <!-- Google Charts API or manual Table to display upcoming events on an account-->
+          <div id="table_div" class="centered-contents-div">
+            <a href="event/index.php?action=edit_event" class="button login w-button">Edit Event</a>
+          </div>
           </div>
         </div>
         <div class="columns w-row">
