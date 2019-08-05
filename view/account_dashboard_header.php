88824ff1c3b91bea76af11bd7edd410e30527f00
Ryan <ryknow0@gmail.com>
2019-08-05 01:24:15 -0600

    account dashboard update 16

--- view/account_dashboard_header.php
+++ view/account_dashboard_header.php
@@ -67,7 +67,7 @@ $ytd_events = 12;
 
         var table = new google.visualization.Table(document.getElementById('table_div'));
 
-        table.draw(data, {showRowNumber: true, width: '100%', height: '50%'});
+        table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
     }
 </script>
 </head>
