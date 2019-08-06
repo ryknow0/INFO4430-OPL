5226e9be67446cd5a30c325d4aa5d7d0140030e4
Ryan <ryknow0@gmail.com>
2019-08-05 23:30:13 -0600

    account dashboard

--- index.php
+++ index.php
@@ -33,18 +33,31 @@ include 'view/account_dashboard_header.php';
           <!-- Google Charts API or manual Table to display upcoming events on an account-->
           <div>
           <?php 
-          function event_table($events = array()){
-            $rows = array();
-            foreach ($events as $row) {
-              $cells = array();
-              foreach ($row as $cell) {
-                  $cells[] = "<td>{$cell}</td>";
-                }
-              $rows[] = "<tr>" . implode('', $cells) . "</tr>";
-            }
-            return "<table class='hci-table'>" . implode('', $rows) . "</table>";
-          }
-          echo event_table($events);
+          function build_table($array){
+    // start table
+    $html = '<table>';
+    // header row
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+    $html .= '</tr>';
+
+    // data rows
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    // finish table and return it
+
+    $html .= '</table>';
+    return $html;
+}
+          echo build_table($events);
           ?>
           </div>
         </div>
