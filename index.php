549aaef73dc7e5594c81cbcac8891da82c481d5c
Ryan <ryknow0@gmail.com>
2019-08-06 01:54:23 -0600

    account dashboard update

--- index.php
+++ index.php
@@ -42,16 +42,22 @@ include 'view/account_dashboard_header.php';
         </div>
       </div>
       <div class="columns w-row">
+        <div class="w-col w-col-4">
           <div class="centered-contents-div">
             <a href="event/index.php?action=create_event" class="button fixed-width-button w-button">Create Event</a>
           </div>
-          <div class="w-col w-col-4 w-col-small-small-stack">
+        </div>
+        <div class="w-col w-col-4">
+          <div class="centered-contents-div">
             <a href="user/index.php?action=create_user" class="button fixed-width-button w-button">Add User</a>
           </div>
-          <div class="w-col w-col-4 w-col-small-small-stack">
+        </div>
+        <div class="w-col w-col-4">
+          <div class="centered-contents-div">
             <a href="report/index.php?action=current_events" class="button fixed-width-button w-button">Generate Report</a>
           </div>
         </div>
+      </div>
     </div>
   </div>
   <div>
